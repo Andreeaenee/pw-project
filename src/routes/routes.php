0a210246a4499ff3b4567b10af9b5ebebc8ea87f
NicoletaFartan02 <nicoleta.fartan02@e-uvt.ro>
2024-04-20 22:54:17 +0300

    Made get route for user searching by user id.

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -9,4 +9,10 @@ return function (App $app) {
 
     // Setup route using the RouteService
     $app->get('/', 'RouteService:helloWorld');
+
+    // Include UserRouteHandler
+    require __DIR__ . '/../services/UserRouteHandler.php';
+
+    // Setup user route using UserRouteHandler
+    $app->get('/user/{id}', 'UserRouteHandler:getUser');
 };
